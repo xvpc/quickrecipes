453a46bb9f79c87bd104ce2050e21950b6dd6478
Viper <viperxx100@gmail.com>
2023-09-16 19:26:46 +0300

    $

--- pages/recipes/[id].tsx
+++ pages/recipes/[id].tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // NextJs
 import { GetStaticPropsContext, GetServerSidePropsContext } from 'next'
+import { useRouter } from 'next/router'
 
 // Components
 import Layout from '@/components/Layout'
@@ -10,11 +11,35 @@ import Info from '@/components/pages/Info'
 // Lib
 import getInfo from '@/lib/getInfo'
 
-export default function InfoPage(props: { data: any }) {
+export default function InfoPage() {
+    const router = useRouter();
+    const [data, setData] = useState<any>(null)
+
+    useEffect(() => {
+        const id = router.query?.id || "";
+
+        (async() => {
+            try{
+                const data = await getInfo({id: String(id)})
+
+                if(data?.error){
+                    console.log(`Error message => ${data?.error}`)
+        
+                    router.push('/404')
+                }else{
+                    setData(data)
+                }
+            }catch(err: any){
+                console.log(`Error from useEffect ${err.message}`)
+        
+                router.push('/')
+            }
+        })()
+    }, [])
     
     return (
         <Layout title=''>
-            <Info data={props.data} />
+            <Info data={data} />
         </Layout>
     )
 }
@@ -26,32 +51,32 @@ export default function InfoPage(props: { data: any }) {
 //     }
 // }
 
-export async function getServerSideProps(context: GetServerSidePropsContext){
-    const id = context.params?.id as string;
+// export async function getServerSideProps(context: GetServerSidePropsContext){
+//     const id = context.params?.id as string;
 
-    // 
-    try{
-        const data = await getInfo({id: id})
-        if(data?.error){
-            console.log(`Error message => ${data?.error}`)
+//     // 
+//     try{
+//         const data = await getInfo({id: id})
+//         if(data?.error){
+//             console.log(`Error message => ${data?.error}`)
 
-            return { 
-                notFound: true
-            };
-        }else{
-            // console.log(data)
+//             return { 
+//                 notFound: true
+//             };
+//         }else{
+//             // console.log(data)
 
-            return { 
-                props: {
-                    data: data // Returns first meal (Object)
-                }
-            };
-        }
-    }catch(err: any){
-        console.log(`Error from getStaticProps ${err.message}`)
+//             return { 
+//                 props: {
+//                     data: data // Returns first meal (Object)
+//                 }
+//             };
+//         }
+//     }catch(err: any){
+//         console.log(`Error from getStaticProps ${err.message}`)
 
-        return { 
-            notFound: true
-        };
-    }
-}
+//         return { 
+//             notFound: true
+//         };
+//     }
+// }
